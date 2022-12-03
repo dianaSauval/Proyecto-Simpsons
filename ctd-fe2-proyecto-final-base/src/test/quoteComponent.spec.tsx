c62fbe92999f23e68e3ed2b591cc73278723ffc5
dianasauval <dianasauval@hotmail.com>
2022-12-03 14:03:43 -0300

    agregado el test del botón borrar

--- ctd-fe2-proyecto-final-base/src/test/quoteComponent.spec.tsx
+++ ctd-fe2-proyecto-final-base/src/test/quoteComponent.spec.tsx
@@ -4,19 +4,30 @@ import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe("Quote", ()=>{
-    describe("If I want a random quote", ()=>{
+    describe("random quote button", ()=>{
         it("should show me the random quote button", async()=>{
             render(<Quote/>)
             expect (screen.getByLabelText("Obtener cita aleatoria")).toBeInTheDocument()
         })
     })
 
-    describe("If I want a specific quote", ()=>{
+    describe("specific quote button", ()=>{
         it("should show me the specific quote button", async()=>{
             render(<Quote/>) 
             const inputAutor = screen.getByLabelText("Author Cita")
             await userEvent.type(inputAutor, "Marge")
             expect (screen.getByLabelText("Obtener Cita")).toBeInTheDocument()
+        })
+    })
+
+    describe("delete button", ()=>{
+        it("should clear the content of the input", async()=>{
+            render(<Quote/>) 
+            const inputAutor = screen.getByLabelText("Author Cita")
+            const deleteButton = screen.getByLabelText("Borrar")
+            await userEvent.type(inputAutor, "Marge")
+            await userEvent.click(deleteButton)
+            expect (screen.getByPlaceholderText("Ingresa el nombre del autor")).toBeInTheDocument()
         })
     })
 
